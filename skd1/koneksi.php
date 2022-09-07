11fd671b6b78b8425ef7c1dafdea85f309a3b5fc
derisr <111950188+derisr@users.noreply.github.com>
2022-09-07 18:42:11 +0700

    Delete skd1 directory

--- skd1/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $koneksi = mysqli_connect('localhost', 'root', '', 'uts_rizkideri');
-
-    if(!$koneksi)
-    {
-        die ("Koneksi dengan database gagal: ".mysqli_connect_errno().mysqli_connect_error());
-    }
-?>
